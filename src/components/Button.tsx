275e6eb1c3dc488439b6be216a9cb0ba58c55660
JalpeshDev <jalpesh.fullstack@gmail.com>
2024-05-15 20:28:40 +0530

    feat: ssg test task

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,23 @@
-import { FunctionComponent, PropsWithChildren } from "react";
+import { FC, PropsWithChildren } from "react";
 import classNames from "classnames";
 
-type ButtonProps = {};
+type ButtonProps = {
+  onClick: () => void;
+  isSelected: boolean;
+};
 
-export const Button: FunctionComponent<PropsWithChildren<ButtonProps>> = ({
+export const Button: FC<PropsWithChildren<ButtonProps>> = ({
+  onClick,
+  isSelected,
   children,
 }) => {
   return (
     <button
       type="button"
-      className={classNames("px-2 py-1 border border-black")}
+      className={classNames("px-2 py-1 border border-black", {
+        "bg-blue-400": isSelected,
+      })}
+      onClick={onClick}
     >
       {children}
     </button>
